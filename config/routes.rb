86eb623326fcb6923473de92da6818421055d485
Aleksandr Nikulenko <11nav09alex1mo@gmail.com>
2018-11-16 21:48:32 +0200

    end chapter 3 on tutorial ruby on rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  root 'static_pages#home'
+
+  get 'help' => 'static_pages#help'
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
 end
